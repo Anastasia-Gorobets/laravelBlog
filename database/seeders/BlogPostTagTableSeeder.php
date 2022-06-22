79759db75c2f97157fd98c26073f1012eb95ff60
nasty <nastya.gorobets95@gmail.com>
2022-06-22 11:29:10 +0300

    Added seeder for blog post tags

---
+++ database/seeders/BlogPostTagTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BlogPost;
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class BlogPostTagTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tagCount = Tag::all()->count();
+        if($tagCount === 0){
+            $this->command->info('No tags found');
+            return;
+        }
+        $howManyMin = (int)$this->command->ask('How many min tags for blog post',0);
+        $howManyMax = min((int)$this->command->ask('How many max tags for blog post',$tagCount),$tagCount);
+        BlogPost::all()->each(function (BlogPost  $blogPost) use ($howManyMin, $howManyMax){
+            $take = random_int($howManyMin, $howManyMax);
+            $tags = Tag::inRandomOrder()->take($take)->get()->pluck('id');
+            $blogPost->tags()->sync($tags);
+        });
+    }
+}
